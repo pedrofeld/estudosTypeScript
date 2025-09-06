80cb1eefd94d4e4c76f8e8329220a4d9fa657ca2
Pedro Feld <pedro.gael.feld@gmail.com>
2025-09-06 15:00:58 -0300

    feat: adição e compilação de elementos primitivos

--- dist/primitivos.js
+++ dist/primitivos.js
@@ -1,9 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 let nome = "Pedro";
 let sobrenome = "Feld";
 console.log(`Nome: ${nome} ${sobrenome}`);
-
 let idade = 21;
 console.log(`Idade: ${idade}`);
-
 let altura = 1.65;
 console.log(`Altura: ${altura}m`);
