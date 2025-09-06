9c5f6372080b69227027321096ff96f1bbf74cd6
Pedro Feld <pedro.gael.feld@gmail.com>
2025-09-06 16:22:32 -0300

    feat: variáveis

---
+++ dist/usuario-idade.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Pedro";
+const age = 21;
+const isAdult = age >= 18;
+if (isAdult) {
+    console.log(`${name} é maior de idade.`);
+}
+else {
+    console.log(`${name} é menor de idade.`);
+}
---
+++ src/usuario-idade.ts
@@ -0,0 +1,9 @@
+const name: string = "Pedro";
+const age: number = 21;
+const isAdult: boolean = age >= 18;
+
+if (isAdult) {
+    console.log(`${name} é maior de idade.`);
+} else {
+    console.log(`${name} é menor de idade.`);
+}
