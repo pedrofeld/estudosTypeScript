793808057ad52ec5a36a9c57d48ca3eb21f3c2b4
Pedro Feld <pedro.gael.feld@gmail.com>
2025-09-08 20:54:20 -0300

    feat: parâmetro rest

--- dist/params.js
+++ dist/params.js
@@ -21,3 +21,27 @@ function greetVisitor3(name, number = 0) {
 }
 greetVisitor3("Ana");
 greetVisitor3("Bia", 23);
+console.log("-----");
+// parâmetro rest
+function greetStudent(escola, ...students) {
+    console.log(`Saudações alunos da escola ${escola}:`);
+    students.forEach(student => {
+        console.log(`- ${student}`);
+    });
+}
+greetStudent("Growdev", "Jessica", "Tiago", "Carlos");
+function sumAll(...numbers) {
+    let i = 0;
+    numbers.forEach(num => {
+        i += num;
+    });
+    return i;
+}
+console.log("-----");
+console.log(sumAll(1, 2, 3, 4, 5));
+console.log("-----");
+const student1 = { name: "Jessica", className: "Turma A" };
+function greetStudent2({ name, className }) {
+    console.log(`Olá, ${name} da turma ${className}.`);
+}
+greetStudent2(student1);
--- src/params.ts
+++ src/params.ts
@@ -28,3 +28,45 @@ function greetVisitor3(name: string, number: number = 0): void {
 
 greetVisitor3("Ana");
 greetVisitor3("Bia", 23);
+
+console.log("-----");
+
+// parâmetro rest
+
+function greetStudent(escola: string, ...students: string[]): void {
+    console.log(`Saudações alunos da escola ${escola}:`);
+    students.forEach(student => {
+        console.log(`- ${student}`);
+    });
+}
+
+greetStudent("Growdev", "Jessica", "Tiago", "Carlos");
+
+function sumAll(...numbers: number[]): number {
+    let i: number = 0;
+
+    numbers.forEach(num => {
+        i += num;
+    });
+
+    return i;
+}
+
+console.log("-----");
+
+console.log(sumAll(1, 2, 3, 4, 5));
+
+interface Student {
+    name: string;
+    className: string;
+}
+
+console.log("-----");
+
+const student1: Student = { name: "Jessica", className: "Turma A" };
+
+function greetStudent2({name, className}: Student): void {
+    console.log(`Olá, ${name} da turma ${className}.`);
+}
+
+greetStudent2(student1);
