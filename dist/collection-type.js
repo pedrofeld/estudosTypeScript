208ea7100c12da689584d909510523c3cec93922
Pedro Feld <pedro.gael.feld@gmail.com>
2025-09-08 19:31:38 -0300

    feat: tipos de coleção (matriz e tupla)

---
+++ dist/collection-type.js
@@ -0,0 +1,12 @@
+"use strict";
+//matriz
+Object.defineProperty(exports, "__esModule", { value: true });
+let list = ["maçã", "banana", "laranja"];
+let list2 = ["maçã", "banana", "laranja"];
+console.log(list);
+console.log(list2);
+let numerosAte10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numerosAte10);
+//tupla
+let pessoa = ["João", 30, true];
+console.log(pessoa);
---
+++ src/collection-type.ts
@@ -0,0 +1,15 @@
+//matriz
+
+let list: string[] = ["maçã", "banana", "laranja"];
+let list2: Array<string> = ["maçã", "banana", "laranja"];
+
+console.log(list);
+console.log(list2);
+
+let numerosAte10: number[] = [0,1,2,3,4,5,6,7,8,9,10];
+console.log(numerosAte10);
+
+//tupla
+let pessoa: [string, number, boolean] = ["João", 30, true];
+
+console.log(pessoa);
